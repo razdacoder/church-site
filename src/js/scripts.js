a6b2a26f9e6c23ac7646a87450b7f6bd4639f779
Ramon Rasheed <ramonrash2@gmail.com>
2024-09-23 12:19:37 +0100

    district

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -68,4 +68,89 @@ $(document).ready(function () {
     // Toggle the icons
     $(this).find("i").toggleClass("fa-plus fa-minus");
   });
+
+  $.getJSON(
+    "https://code.highcharts.com/mapdata/countries/ng/ng-all.topo.json",
+    function (topology) {
+      // Prepare data with each state assigned to its zone
+      var data = [
+        // South West Zone
+        { "hc-key": "ng-og", zone: "South West", color: "#00FF00" }, // Green
+        { "hc-key": "ng-os", zone: "South West", color: "#00FF00" },
+        { "hc-key": "ng-oy", zone: "South West", color: "#00FF00" },
+        { "hc-key": "ng-ek", zone: "South West", color: "#00FF00" },
+        { "hc-key": "ng-la", zone: "South West", color: "#00FF00" },
+        { "hc-key": "ng-on", zone: "South West", color: "#00FF00" },
+
+        // South East Zone
+        { "hc-key": "ng-ab", zone: "South East", color: "#FF0000" }, // Red
+        { "hc-key": "ng-an", zone: "South East", color: "#FF0000" },
+        { "hc-key": "ng-eb", zone: "South East", color: "#FF0000" },
+        { "hc-key": "ng-im", zone: "South East", color: "#FF0000" },
+        { "hc-key": "ng-en", zone: "South East", color: "#FF0000" },
+
+        // North West Zone
+        { "hc-key": "ng-kt", zone: "North West", color: "#0000FF" }, // Blue
+        { "hc-key": "ng-za", zone: "North West", color: "#0000FF" },
+        { "hc-key": "ng-ji", zone: "North West", color: "#0000FF" },
+        { "hc-key": "ng-ka", zone: "North West", color: "#0000FF" },
+        { "hc-key": "ng-kg", zone: "North West", color: "#0000FF" },
+        { "hc-key": "ng-so", zone: "North West", color: "#0000FF" },
+
+        // North East Zone
+        { "hc-key": "ng-ad", zone: "North East", color: "#FF00FF" }, // Magenta
+        { "hc-key": "ng-ba", zone: "North East", color: "#FF00FF" },
+        { "hc-key": "ng-bo", zone: "North East", color: "#FF00FF" },
+        { "hc-key": "ng-go", zone: "North East", color: "#FF00FF" },
+        { "hc-key": "ng-yo", zone: "North East", color: "#FF00FF" },
+        { "hc-key": "ng-ta", zone: "North East", color: "#FF00FF" },
+
+        // North Central Zone
+        { "hc-key": "ng-ni", zone: "North Central", color: "#FFFF00" }, // Yellow
+        { "hc-key": "ng-be", zone: "North Central", color: "#FFFF00" },
+        { "hc-key": "ng-ko", zone: "North Central", color: "#FFFF00" },
+        { "hc-key": "ng-kw", zone: "North Central", color: "#FFFF00" },
+        { "hc-key": "ng-na", zone: "North Central", color: "#FFFF00" },
+        { "hc-key": "ng-pl", zone: "North Central", color: "#FFFF00" },
+        { "hc-key": "ng-fc", zone: "North Central", color: "#FFFF00" }, // FCT
+
+        // South South Zone
+        { "hc-key": "ng-ak", zone: "South South", color: "#00FFFF" }, // Cyan
+        { "hc-key": "ng-ba", zone: "South South", color: "#00FFFF" },
+        { "hc-key": "ng-cr", zone: "South South", color: "#00FFFF" },
+        { "hc-key": "ng-de", zone: "South South", color: "#00FFFF" },
+        { "hc-key": "ng-ed", zone: "South South", color: "#00FFFF" },
+        { "hc-key": "ng-ri", zone: "South South", color: "#00FFFF" },
+      ];
+
+      // Create the chart
+      Highcharts.mapChart("district-map", {
+        chart: {
+          map: topology,
+          backgroundColor: "#f2ead1",
+        },
+
+        title: {
+          text: "",
+        },
+
+        series: [
+          {
+            name: "Zones",
+            data: data,
+            states: {
+              hover: {
+                color: "#BADA55", // Highlight on hover
+              },
+            },
+            dataLabels: {
+              enabled: true,
+              format: "{point.name}", // Shows the name of each state
+            },
+            colorKey: "color", // Use 'color' field in data to color the map
+          },
+        ],
+      });
+    }
+  );
 });
